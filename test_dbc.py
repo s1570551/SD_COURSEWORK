3ff8ee052971acc657f9a740056ef3039ec138fe
nick <s1570551@sms.ed.ac.uk>
2016-03-25 10:57:35 +0000

    add some test case

--- test_dbc.py
+++ test_dbc.py
@@ -42,13 +42,57 @@ class DBCTestCase(unittest.TestCase):
         self.assertEquals(dbc.show_cards(supplement), 0,
                           msg="Error occurs in show card function")
 
+    def test_init_new_game(self):
+        self.assertEquals(dbc.init_new_game(player_human, player_computer, test_central), 0,
+                          msg="Fail to initialize new game")
+
     def test_input_check(self):
-        self.assertEquals(dbc.input_check('Y',1), 0,
+        self.assertEquals(dbc.input_check('y',1), 0,
                           msg="Check input type 1 failed")
+        self.assertEquals(dbc.input_check(';',1), 1,
+                          msg="Check input type 1 failed")
+        self.assertEquals(dbc.input_check('q',2), 0,
+                          msg="Check input type 2 failed")
         self.assertEquals(dbc.input_check('Y',2), 1,
                           msg="Check input type 2 failed")
         self.assertEquals(dbc.input_check('3',3), 0,
-                          msg="Check input type 1 failed")
+                          msg="Check input type 3 failed")
+        self.assertEquals(dbc.input_check('Y',3), 1,
+                          msg="Check input type 3 failed")
+        self.assertEquals(dbc.input_check('E',4), 0,
+                          msg="Check input type 3 failed")
+        self.assertEquals(dbc.input_check('p',4), 1,
+                          msg="Check input type 3 failed")
+
+    def test_purchase_supplement(self):
+        dbc.init_player_info(player_human)
+        dbc.init_central_deck(test_central)
+
+        self.assertEquals(dbc.purchase_supplement(player_human,test_central), 0,
+                          msg = "User purchase supplement failed")
+
+    def test_play_all_cards(self):
+        dbc.init_new_game(player_human, player_computer, test_central)
+        dbc.draw_cards(player_human)
+        result = dbc.play_all_cards(player_human)
+        self.assertEquals(result, [3, 2],
+                          msg="Error occurs in playing all cards %s" %result)
+
+    def test_discard_cards(self):
+        dbc.init_new_game(player_human, player_computer, test_central)
+        dbc.draw_cards(player_human)
+        result = dbc.discard_cards(player_human)
+        self.assertEquals(result, 0,
+                          msg="Error occurs in discarding cards %s" %result)
+
+    def test_will_continue(self):
+        dbc.init_new_game(player_human, player_computer, test_central)
+        player_human['health'] = -2
+        result = dbc.will_continue(player_human, player_computer, test_central)
+        self.assertEquals(result, False,
+                          msg="Error occurs when judging winner %s" %result)
+
+
 
 
 
